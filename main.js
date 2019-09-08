d0fb2e5d2e7766cfe01a776f11b4f38816654e9f
RamosJennifer <jenniferramos4858@gmail.com>
2019-09-08 15:39:32 -0500

    pseudocoding

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create an array "topic" and add 5 string values
+
+// Try using a loop that appends a button for each string in the array.
+
+// Create an on-click event that displays 10 corresponding ratings and images (gifs)
+
+// Push button objects into the "topic" array
